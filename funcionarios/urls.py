78bed3f0b203379ed6304af94783b5506a2efdbf
Pedro Deffanti <pedrodeffantijr@gmail.com>
2022-08-12 16:04:01 -0300

    Criação de tela inicial

--- funcionarios/urls.py
+++ funcionarios/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import FuncionarioViews
+from .views import FuncionarioViews, create, store, home
 
 urlpatterns = [
     path('funcionarios/', FuncionarioViews),
+    path('', home),
+    path('create/', create),
+    path('store/', store),
 ]
--- funcionarios/views.py
+++ funcionarios/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from funcionarios.forms import FuncionarioForms
 
@@ -17,4 +18,23 @@ def FuncionarioViews(request):
         context = {
             'form': form
         }
-        return render(request, 'funcionarios.html')
+        return render(request, 'funcionario.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def create(request):
+    return render(request, 'create.html')
+
+def store(request):
+    data = {}
+    if(request.POST['password'] != request.POST['password-conf']):
+        data['msg'] = 'Senha e conformação de senha diferentes!'
+        data['class'] = 'alert-danger'
+    else:
+        user = User.objects.create_user(request.POST['name'], request.POST['email'], request.POST['password'])
+        user.first_name = request.POST['name']
+        user.save()
+        data['msg'] = 'Usuário Cadastrado com Sucesso'
+        data['class'] = 'alert-sucess'
+    return render(request, 'create.html', data)
